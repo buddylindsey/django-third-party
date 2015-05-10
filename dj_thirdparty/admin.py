4d690a73a40d02b4c47a9a1002763552c6f9b3f4
Buddy Lindsey <blindsey@consumeraffairs.com>
2015-05-09 21:58:17 -0500

    add admin

---
+++ dj_thirdparty/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import CustomContent
+
+
+class CustomJavascriptAdmin(admin.ModelAdmin):
+    list_display = ('title', 'path', 'active')
+    list_filter = ('active', 'exact_match', 'partial_match')
+    fieldsets = (
+        (None, {'fields': ('title', 'path',)}),
+        ('Settings', {'fields': (
+            'active', 'exact_match', 'partial_match', 'header',)}),
+        ('Content', {'fields': ('javascript', 'css')})
+    )
+
+    def activate(self, request, qs):
+        qs.update(active=True)
+    activate.short_description = 'Activate selected javascript'
+
+    def deactivate(self, request, qs):
+        qs.update(active=False)
+    deactivate.short_description = 'Deactivate selected javascript'
+
+    actions = (activate, deactivate)
+
+
+admin.site.register(CustomJavascript, CustomJavascriptAdmin)
